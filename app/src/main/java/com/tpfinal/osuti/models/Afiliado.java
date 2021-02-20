60042eea81045842045069496a7ffa53fcfc7e47
lucialeitessanchez <lucialeitessanchez@gmail.com>
2021-02-19 21:02:44 -0300

    listo afiliaciones

--- app/src/main/java/com/tpfinal/osuti/models/Afiliado.java
+++ app/src/main/java/com/tpfinal/osuti/models/Afiliado.java
@@ -1,4 +1,60 @@
 package com.tpfinal.osuti.models;
 
 public class Afiliado {
+    private String nombre;
+    private String apellido;
+    private Integer DNI;
+    private String fechaNac;
+    private String email;
+
+    public Afiliado() {
+    }
+
+    public Afiliado(String nombre, String apellido, Integer DNI, String fechaNac, String email) {
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.DNI = DNI;
+        this.fechaNac = fechaNac;
+        this.email = email;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public Integer getDNI() {
+        return DNI;
+    }
+
+    public void setDNI(Integer DNI) {
+        this.DNI = DNI;
+    }
+
+    public String getFechaNac() {
+        return fechaNac;
+    }
+
+    public void setFechaNac(String fechaNac) {
+        this.fechaNac = fechaNac;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
 }
--- app/src/main/java/com/tpfinal/osuti/ui/afiliaciones/AfiliacionFragment.java
+++ app/src/main/java/com/tpfinal/osuti/ui/afiliaciones/AfiliacionFragment.java
@@ -1,34 +1,103 @@
 package com.tpfinal.osuti.ui.afiliaciones;
 
+import android.app.ActionBar;
+import android.app.Notification;
+import android.content.Context;
+import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.os.SystemClock;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.SeekBar;
 import android.widget.TextView;
+import android.app.NotificationManager;
+import android.content.BroadcastReceiver;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.core.app.NotificationCompat;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 
+import com.tpfinal.osuti.MainActivity;
 import com.tpfinal.osuti.R;
+import com.tpfinal.osuti.models.Afiliado;
 
 public class AfiliacionFragment extends Fragment {
 
     private AfiliacionViewModel afiliacionViewModel;
+    private EditText nombre,apellido,email,dni,fechanac;
+    private Button botonAfiliar;
+    public Afiliado afiliado;
+    private ProgressBar progressBar;
+    private Notificacion notificacion;
+
+
+    public final static String CHANNEL_ID = "NOTIFICACION";
+    private final static int NOTIFICACION_ID = 1;
+
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
+
         afiliacionViewModel = new ViewModelProvider(this).get(AfiliacionViewModel.class);
         View root = inflater.inflate(R.layout.fragment_afiliaciones, container, false);
-        final TextView textView = root.findViewById(R.id.text_afiliacion);
-        afiliacionViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
+
+        nombre=(EditText)root.findViewById(R.id.etNombreaf);
+        apellido=(EditText)root.findViewById(R.id.etApellidoaf);
+        email=(EditText)root.findViewById(R.id.etEmailaf);
+        dni=(EditText)root.findViewById(R.id.etDNIaf);
+        fechanac=(EditText)root.findViewById(R.id.etFehanacaf);
+        botonAfiliar=(Button)root.findViewById(R.id.buttonAfiliacion);
+        progressBar=(ProgressBar)root.findViewById(R.id.progressBar);
+
+        progressBar.setVisibility(View.GONE);
+        Intent i;
+
+
+
+        Context ctx;
+        ctx=getActivity();
+
+        botonAfiliar.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onChanged(@Nullable String s) {
-                textView.setText(s);
+            public void onClick(View v) {
+                afiliado.setNombre(nombre.toString());
+                afiliado.setApellido(apellido.toString());
+                afiliado.setDNI(Integer.parseInt(dni.toString()));
+                afiliado.setFechaNac(fechanac.toString());
+
+
+                SystemClock.sleep(7000);
+                progressBar.setVisibility(View.VISIBLE);
+
+                //no se como llamarla
+                Notification notification = new NotificationCompat.Builder(ctx, CHANNEL_ID)
+                        .setSmallIcon(R.drawable.logo_app)
+                        .setContentTitle("Usted es nuevo afiliado")
+                        .setContentText("Felicitaciones se le enviara un email con su numero de socio")
+                        .setStyle(new NotificationCompat.BigPictureStyle()
+                                .bigLargeIcon(null))
+                        .build();
+
             }
         });
+
+
+        i=new Intent(getActivity(),MainActivity.class);
+        startActivity(i);
+
+
+
+
         return root;
     }
+
+
 }
